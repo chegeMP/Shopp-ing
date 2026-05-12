bb9f45b2b9fef85c902958573ab096b83e1dc25b
MarkPaul Chege <markpaul.chege@jtl.co.ke>
2026-05-12 12:39:08 +0300

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { BasketProvider } from "@/components/BasketContext";
@@ -89,6 +90,7 @@ export default async function RootLayout({
             </BasketProvider>
           </ThemeProvider>
         </CatalogProvider>
+        <Analytics />
       </body>
     </html>
   );
